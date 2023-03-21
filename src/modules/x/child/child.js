05e7115dc214d778d9a02a07297788a167140b0b
divmain <dale@divmain.com>
2023-03-21 16:48:55 -0700

    3 files modified

--- src/modules/x/child/child.js
+++ src/modules/x/child/child.js
@@ -1,6 +1,8 @@
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
+  static validationOptOut = ['class'];
+
   connectedCallback() {
     this.classList.add('foo');
   }
