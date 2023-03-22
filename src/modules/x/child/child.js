77f09e005a6aca45eee3dcf8a016da3847c2bde7
divmain <dale@divmain.com>
2023-03-21 17:03:01 -0700

    1 file modified

--- src/modules/x/child/child.js
+++ src/modules/x/child/child.js
@@ -1,6 +1,8 @@
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
+  // This ⤵ is also permitted, but will disable all validation for the component.
+  // static validationOptOut = true
   static validationOptOut = ['class'];
 
   connectedCallback() {
